fafe98b03cdeedc51f00135db8199666ec60f314
github username <github email address>
2019-09-10 10:44:39 -0700

    cards have content fitting into the container and headings are prominent but still being adjusted.

--- front-end/project-front-end/src/components/AnimalHeader.js
+++ front-end/project-front-end/src/components/AnimalHeader.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 function AnimalHeader(props) {
   return (
     <div className="animal-header">
-      <Link to="/mammals">Mammals</Link>| <Link to="/fish">Fish</Link>|{" "}
-      <Link to="/reptiles">Reptiles</Link>|{" "}
-      <Link to="/amphibians">Amphibians</Link>| <Link to="/birds">Birds</Link>
+      <Link to="/mammals" className="link">Mammals | </Link>
+      <Link to="/fish" className="link">Fish | </Link>
+      <Link to="/reptiles" className="link">Reptiles | </Link>
+      <Link to="/amphibians" className="link">Amphibians | </Link>
+      <Link to="/birds" className="link">Birds</Link>
     </div>
   );
 }
