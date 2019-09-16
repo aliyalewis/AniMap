3d874ce355fddeaab22e35214dd7e125ea39ee34
github username <github email address>
2019-09-16 15:24:37 -0700

    card spacing is fixed.

--- front-end/project-front-end/src/components/CreateNote.js
+++ front-end/project-front-end/src/components/CreateNote.js
@@ -35,7 +35,7 @@ class CreateNote extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input className="input" name="title" type="text" placeholder="Title" onChange={this.handleChange}/>
+          <input className="title-input" name="title" type="text" placeholder="Title" onChange={this.handleChange}/>
           <input className="input" name="body" type="text" placholder="Write your note here" onChange={this.handleChange}/>
           <button className="button" type="submit">Create a new note</button>
         </form>
